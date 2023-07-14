5b055d1e9e16efede5ac2f7513884d4defed2ed7
Isra1997 <israragheb@gmail.com>
2023-07-14 21:55:21 +0300

    first commit

---
+++ src/main/java/com/Device/ConfigurationSoftware/ConfigurationSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.Device.ConfigurationSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Device/ConfigurationSoftware/ConfigurationSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Device.ConfigurationSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
