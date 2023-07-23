c2e0d1c5ee1507352d7f6f48bfd1cb28e7ae043c
Ragheb, Isra <Isra.Ragheb@dell.com>
2023-07-23 18:30:05 +0300

    fix: used JPA instead of Crud

--- src/main/java/com/Device/ConfigurationSoftware/repository/DeviceRepository.java
+++ src/main/java/com/Device/ConfigurationSoftware/repository/DeviceRepository.java
@@ -1,9 +1,10 @@
 package com.Device.ConfigurationSoftware.repository;
 
 import com.Device.ConfigurationSoftware.entity.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DeviceRepository extends CrudRepository<Device, Long> {
+public interface DeviceRepository extends JpaRepository<Device, Long> {
 }
--- src/test/java/com/Device/ConfigurationSoftware/controller/DeviceControllerTest.java
+++ src/test/java/com/Device/ConfigurationSoftware/controller/DeviceControllerTest.java
@@ -1,44 +1,123 @@
 package com.Device.ConfigurationSoftware.controller;
 
-import com.Device.ConfigurationSoftware.TestUtilities;
-import com.Device.ConfigurationSoftware.dto.DeviceDTO;
 import com.Device.ConfigurationSoftware.entity.Device;
-import com.Device.ConfigurationSoftware.repository.DeviceRepository;
-import junit.framework.TestCase;
+import com.Device.ConfigurationSoftware.exceptions.DeviceException;
+import com.Device.ConfigurationSoftware.service.DeviceService;
+import com.Device.ConfigurationSoftware.service.EncryptionService;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.http.ResponseEntity;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.mock;
 
-public class DeviceControllerTest extends TestCase {
+import java.util.Collections;
+import java.util.Optional;
 
-    private DeviceRepository deviceRepository = mock(DeviceRepository.class);
-    private DeviceController deviceController;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+public class DeviceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<Device> json;
+
+    @MockBean
+    DeviceService deviceService;
+
+    @MockBean
+    EncryptionService encryptionService;
 
     @Before
-    public void setup(){
-        deviceController = new DeviceController();
-        TestUtilities.injectObject(deviceController,"deviceRepository",deviceRepository);
+    public void setup() throws DeviceException {
+        Device device = new Device();
+        device.setId(1L);
+        Device configuredDevice = new Device();
+        configuredDevice.setStatus("ACTIVE");
+        configuredDevice.setTemperature(1);
+        given(deviceService.addDevice(any())).willReturn(device);
+        given(deviceService.getDeviceById(any())).willReturn(Optional.of(device));
+        given(deviceService.configureDevice(any())).willReturn(configuredDevice);
+        given(deviceService.updateDevice(any())).willReturn(device);
+        given(deviceService.getAllDevices()).willReturn(Collections.emptyList());
     }
 
     @Test
-    public void test_happy_addDevice(){
-        //Arrange
-        Device mockDevice = new Device();
-        mockDevice.setPin_code("1234567");
-        mockDevice.setTemperature(1);
-        mockDevice.setStatus("READY");
-        //Act
-        ResponseEntity<DeviceDTO> createdDevice = deviceController.addDevice(mockDevice);
-        //Assert
-        assertNotNull(createdDevice);
-        assertEquals(200,createdDevice.getStatusCode());
-        DeviceDTO device = createdDevice.getBody();
-        assertEquals(device.getDevices().get(0).getPin_code(),1234567);
-        assertEquals(device.getDevices().get(0).getTemperature(),1);
-        assertEquals(device.getDevices().get(0).getStatus(),"READY");
+    public void getAllDevices() throws Exception {
+        mockMvc.perform(get("/device"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("{\"devices\":[],\"message\":null}"));
+
+        verify(deviceService,times(1)).getAllDevices();
+    }
+
+    @Test
+    public void getAddDevice() throws Exception {
+        Device mockDevice = getDevice();
+        mockMvc.perform(post("/device")
+                .content(json.write(mockDevice).getJson())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void deleteDevice() throws Exception {
+        mockMvc.perform(delete("/device/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                       .andExpect(status().isOk());
+    }
+
+    @Test
+    public void editDevice() throws Exception {
+        Device mockDevice = getDevice();
+        mockMvc.perform(put("/device")
+                        .content(json.write(mockDevice).getJson())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 
+
+    @Test
+    public void configureDevice() throws Exception {
+        mockMvc.perform(post("/configure/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+
+    private Device getDevice() {
+        Device device = new Device();
+        device.setPin_code("1234567");
+        device.setStatus("READY");
+        device.setTemperature(1);
+        device.setId(1L);
+        return device;
+    }
 }
